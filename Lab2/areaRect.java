db7f71f3166249b1d6a5bd7f325d8550f28ffb5c
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-06-12 13:23:29 +0530

    Lab2 p1

---
+++ Lab2/areaRect.java
@@ -0,0 +1,9 @@
+class arearect {
+    public static void main(String args[]) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int area = a * b;
+        System.out.println("Area of Rectangle is: " + area);
+    }
+}
