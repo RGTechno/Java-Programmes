3fea70b7d7dc8de6e404caa99ca49db2226d3886
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-06-29 12:44:03 +0530

    p2

---
+++ LABFILE/p2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class p2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            int size;
+
+            System.out.println("Enter the size of array: ");
+
+            size = sc.nextInt();
+
+            int[] arr = new int[size];
+            System.out.println("Enter Array Elements:");
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+            for (int i = 0; i < size - 1; i++) {
+                for (int j = 0; j < size - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < size; i++) {
+                System.out.println(arr[i] + " ");
+            }
+        } finally {
+            sc.close();
+        }
+
+    }
+}
